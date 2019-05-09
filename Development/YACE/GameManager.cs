350539c598049613a195b89e69d3d32c965346ca
Anton Roy <anton.francis.roy@gmail.com>
2019-05-09 21:30:29 +0200

    Going back to the project

---
+++ Development/YACE/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YACE
+{
+    class GameManager
+    {
+        public GameContext Context;
+
+        public void PlayCard(int cardId);
+        public void PlayerEndTurn();
+        public void PlayerBuyCard(int cardId);
+    }
+}
---
+++ Development/YACE/YACE.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace YACE
+{
+    class YACE
+    {
+
+    }
+
+    struct RessourcePool
+    {
+        public RessourceDefinition Definition;
+    }
+
+    public struct RessourceDefinition
+    {
+        public string Name;
+        public int MinValue = int.MinValue;
+        public int MaxValue = int.MaxValue;
+        public int BaseValue = 0;
+
+        public bool IsBoundToPlayer = true;
+        public bool ConsumeOnUse = true;
+
+        string[] ResetOnPhase = { };
+    }
+}
