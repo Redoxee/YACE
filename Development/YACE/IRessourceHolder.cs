dce1e6009931ad3f2c9de877f719dd67e2250dc9
Redoxee <anton.francis.roy@gmail.com>
2019-06-03 08:30:32 +0200

    Working on stuff, not really achieving anything

---
+++ Development/YACE/IRessourceHolder.cs
@@ -0,0 +1,10 @@
+﻿namespace YACE
+{
+    interface IRessourceHolder
+    {
+        bool HasRessource(string ressource);
+        int GetRessourcevalue(string ressource);
+        void SetRessource(string ressource, int value);
+        void AddRessource(string ressource, int delta);
+    }
+}
